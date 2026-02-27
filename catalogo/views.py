19efed7d972014d71d5469b38403282e4079fb7d
Armando Giordani Trassi <2024105100018@aluno.ifro.edu.br>
2026-02-26 20:33:03 -0400

    fix urls

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Producto
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        cont = super(HomeView, self).get_context_data(**kwargs)
+        cont['prods'] = Producto.disponivel.all().order_by('?')[:3]
+        return cont

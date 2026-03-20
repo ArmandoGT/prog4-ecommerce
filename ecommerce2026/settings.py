835f15b56c9aee2f08bf78dc1a842f9ccd6839ac
Armando Giordani Trassi <2024105100018@aluno.ifro.edu.br>
2026-03-19 22:12:12 -0400

    aula 190326 inacabado

--- ecommerce2026/settings.py
+++ ecommerce2026/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'catalogo',
     'carrinho'
+    'usuarios  '
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },

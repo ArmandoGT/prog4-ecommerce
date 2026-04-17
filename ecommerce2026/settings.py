7e13d82006e3f9710b94ffc493dd2531de77fb3e
Armando Giordani Trassi <2024105100018@aluno.ifro.edu.br>
2026-04-16 20:25:13 -0400

    Alteração ecommerce2026

--- ecommerce2026/settings.py
+++ ecommerce2026/settings.py
@@ -9,24 +9,27 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 import environ
-
-env = environ.Env()
-environ.Env.read_env(env_file='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ["*"]
+
 
 # Application definition
 
@@ -39,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalogo',
-    'carrinho'
-    'usuarios  '
+    'carrinho',
+    'usuario',
+    'pedido',
+    'pagamento',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
+                'carrinho.context_processors.carrinho',
             ],
         },
     },
@@ -81,11 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DBNAME'),
-        'HOST': env('DBHOST'),
-        'USER': env('DBUSER'),
-        'PASSWORD': env('DBPASS'),
-        'PORT': env('DBPORT'),
-
+        'HOST': env('HOST'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'PORT': env('PORT'),
     }
 }
 
@@ -125,9 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT =  BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "catalogo" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -135,3 +143,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CAR_SESSION_ID = env('CAR_SESSION_ID')
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+
+BRAINTREE_MERCHANT_ID = env('MERCHANT_ID')
+BRAINTREE_PUBLIC_KEY = env('PUBLIC_KEY')
+BRAINTREE_PRIVATE_KEY = env('PRIVATE_KEY')

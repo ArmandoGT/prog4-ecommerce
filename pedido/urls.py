004d069fcffe0fdbc1ce6097c0ac353be2bbe7e7
ArmandoGT <armandogiordani7@gmail.com>
2026-04-16 21:23:53 -0400

    Pedido

---
+++ pedido/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('novo/', views.PedidoCreateView.as_view(),
+         name='novopedido'),
+    path('meuspedidos/', views.PedidoListView.as_view(),
+         name='meuspedidos'),
+    path('pdfmeuspedidos/', views.ListaPedidosPDF.as_view(),
+         name='meuspedidospdf'),
+    path('resumo/<int:idpedido>', views.ResumoPedidoTemplateView.as_view(),
+         name='resumopedido'),
+]
